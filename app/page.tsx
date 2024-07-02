2c0f15910b1d673a3092bd35a88eec9a87cdb45e
monkeymango55 <90772792+sophizhao@users.noreply.github.com>
2024-07-02 18:34:02 -0400

    Removed no cors

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ const Page = () => {
 
   useEffect(() => {
     fetch("http://172.26.177.208:5500/heartbeat/by_time/imu?start_time=-1d&stop_time=-1h", {
-      method: "GET",  mode: 'no-cors'})
+      method: "GET"})
       .then((response) => {
         console.log(response);
         response.json();
